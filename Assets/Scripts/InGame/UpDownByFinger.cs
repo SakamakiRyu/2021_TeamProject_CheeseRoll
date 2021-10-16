ae9d6c60cefc355ea90fceeb2d281c8ddf085e3f
kojima0725 <kojikoji0725kt@gmail.com>
2021-10-16 22:26:06 +0900

    スクリプト追加、Gameシーンの中身を作成

---
+++ Assets/Scripts/InGame/UpDownByFinger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDownByFinger : MonoBehaviour
+{
+    //オブジェクトのy座標の下限値
+    [SerializeField]
+    private float _min;
+
+    //オブジェクトのy座標の上限値
+    [SerializeField]
+    private float _max;
+}
