4c69e012f60d53e41693e6affe9e2864b9e2e44f
kojima0725 <kojikoji0725kt@gmail.com>
2021-10-16 22:45:25 +0900

    RoadMaker

---
+++ Assets/Scripts/InGame/RoadMaker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadMaker : MonoBehaviour
+{
+    //生成したオブジェクトの収納先
+    [SerializeField]
+    private Transform _container;
+    [SerializeField]
+    private float _splitTime;
+
+    private float _timer;
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer > _splitTime)
+        {
+            _timer -= _splitTime;
+            Split();
+        }
+        StretchRoad();
+    }
+
+    private void Split()
+    {
+
+    }
+
+    private void StretchRoad()
+    {
+
+    }
+}
---
+++ Assets/Scripts/InGame/StageMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// ステージを動かす
+/// </summary>
+public class StageMover : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed;
+
+    private void Update()
+    {
+        Vector3 pos = transform.localPosition;
+        pos.z += _moveSpeed * Time.deltaTime;
+        transform.localPosition = pos;
+    }
+}
