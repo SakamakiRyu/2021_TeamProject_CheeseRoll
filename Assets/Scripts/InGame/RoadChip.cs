ac571e8c8ff72d789e93d64639867b11cb7f5c44
kojima0725 <kojikoji0725kt@gmail.com>
2021-10-17 18:31:49 +0900

    鉄板生成

---
+++ Assets/Scripts/InGame/RoadChip.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshCollider))]
+public class RoadChip : MonoBehaviour
+{
+    [SerializeField]
+    MeshFilter _meshFilter;
+    [SerializeField]
+    MeshCollider _meshCollider;
+
+    public void Init(float width, float height)
+    {
+        //メッシュ生成
+        Mesh mesh = new Mesh();
+
+        //頂点の配列
+        Vector3[] vertices = new Vector3[8];
+        //手前の左下、右下、左上、右上
+        vertices[0] = new Vector3(-width, -height, 0);
+        vertices[1] = new Vector3(width, -height, 0);
+        vertices[2] = new Vector3(-width, height, 0);
+        vertices[3] = new Vector3(width, height, 0);
+        //奥のの左下、右下、左上、右上
+        vertices[4] = new Vector3(-width, -height, 0);
+        vertices[5] = new Vector3(width, -height, 0);
+        vertices[6] = new Vector3(-width, height, 0);
+        vertices[7] = new Vector3(width, height, 0);
+
+        mesh.vertices = vertices;
+
+        //uv設定(あとでかく)
+
+        //三角形
+        mesh.triangles = new int[] {1, 3, 7, 7, 5, 1,
+                                    3, 2, 6, 6, 7, 3};
+
+        //領域と法線の計算
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+        _meshFilter.mesh = mesh;
+        //下のは極端に小さいとエラーになるらしい
+        //_meshCollider.sharedMesh = mesh;
+
+    }
+
+    public Vector3[] GetVertices()
+    {
+        return _meshFilter.mesh.vertices;
+    }
+
+    public void SetVertices(Vector3[] vertices)
+    {
+        Mesh mesh = _meshFilter.mesh;
+        mesh.vertices = vertices;
+        //領域と法線の計算
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+        _meshFilter.mesh = mesh;
+        _meshCollider.sharedMesh = mesh;
+    }
+}
--- Assets/Scripts/InGame/RoadMaker.cs
+++ Assets/Scripts/InGame/RoadMaker.cs
@@ -9,27 +9,57 @@ public class RoadMaker : MonoBehaviour
     private Transform _container;
     [SerializeField]
     private float _splitTime;
+    [SerializeField]
+    private RoadChip _roadPrefab;
+    [SerializeField]
+    private float _roadSizeX;
+    [SerializeField]
+    private float _roadSizeY;
 
     private float _timer;
+    private RoadChip _cullentChip;
+
+    private void Start()
+    {
+        MakeNewChip();
+    }
 
     private void Update()
     {
+        StretchRoad();
         _timer += Time.deltaTime;
         if (_timer > _splitTime)
         {
             _timer -= _splitTime;
             Split();
         }
-        StretchRoad();
     }
 
     private void Split()
     {
-
+        MakeNewChip();
     }
 
     private void StretchRoad()
     {
+        Vector3[] vertices = _cullentChip.GetVertices();
+        Vector3 diffPos = this.transform.position - _cullentChip.transform.position;
+        float length = diffPos.z;
+        float height = diffPos.y;
 
+        vertices[4] = new Vector3(-_roadSizeX, -_roadSizeY + height, length);
+        vertices[5] = new Vector3(_roadSizeX, -_roadSizeY + height, length);
+        vertices[6] = new Vector3(-_roadSizeX, _roadSizeY + height, length);
+        vertices[7] = new Vector3(_roadSizeX, _roadSizeY + height, length);
+
+        _cullentChip.SetVertices(vertices);
+    }
+
+    private void MakeNewChip()
+    {
+        RoadChip newChip = Instantiate(_roadPrefab, _container);
+        newChip.transform.position = this.transform.position;
+        newChip.Init(_roadSizeX, _roadSizeY);
+        _cullentChip = newChip;
     }
 }
