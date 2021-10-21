d44b25eebca3685ced1ac4256be8ab242f5bbf88
SakamakiRyu <potesara031w@gmail.com>
2021-10-21 11:27:31 +0900

    UpDownByFingerの修正をした

--- Assets/Scripts/InGame/UpDownByFinger.cs
+++ Assets/Scripts/InGame/UpDownByFinger.cs
@@ -15,14 +15,15 @@ public class UpDownByFinger : MonoBehaviour
     [SerializeField]
     private float _max;
 
-    // 位置座標
-    private Vector3 _pos;
+    [Header("変化量の倍率")]
+    [SerializeField]
+    private float _variation = 1;
 
-    // スクリーン座標をワールド座標に変換した位置座標
-    private Vector3 _screenToWorldPointPosition;
+    /// <summary>前のフレームのy座標を保存しておく</summary>
+    private float _prevMousePosY;
 
     /// <summary>画面に触っているかのフラグ</summary>
-    private bool _isTouch = default;
+    private bool _isTouched = default;
 
     private void Update()
     {
@@ -32,32 +33,45 @@ public class UpDownByFinger : MonoBehaviour
     /// <summary>スマホの上下のスワイプ操作にて、オブジェクトの座標を変更する(↑にスワイプ=上昇)</summary>
     void Control()
     {
+        Vector3 pos;
+        Vector3 screenToWorldPointPosition;
+
         if (Input.GetButtonDown("Fire1"))
         {
-            _isTouch = true;
+            _isTouched = true;
+            // Vector3でマウスの位置座標を取得
+            pos = Input.mousePosition;
+            // Z軸修正
+            pos.z = 10f;
+            // マウス位置座標からスクリーン座標に変換する
+            screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(pos);
+            // タッチしたフレーム時の座標を保存
+            _prevMousePosY = screenToWorldPointPosition.y;
         }
+
+        // 画面に触れていない場合は何もしない
+        if (!_isTouched) return;
 
         if (Input.GetButtonUp("Fire1"))
         {
-            _isTouch = false;
+            _isTouched = false;
+            return;
         }
 
-        if (_isTouch)
+        if (_isTouched)
         {
             // Vector3でマウスの位置座標を取得
-            _pos = Input.mousePosition;
+            pos = Input.mousePosition;
             // Z軸修正
-            _pos.z = 10f;
+            pos.z = 10f;
             // マウス位置座標からスクリーン座標に変換する
-            _screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(_pos);
-
-            // ワールド座標に変換されたマウス座標を代入
-            if (_screenToWorldPointPosition.y > _min && _screenToWorldPointPosition.y < _max)
-            {
-                this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x,
-                _screenToWorldPointPosition.y, this.transform.position.z);
-                Debug.Log($"{_screenToWorldPointPosition.y}");
-            }
+            screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(pos);
+            // 前フレームとの差分を保存する
+            var difference = (screenToWorldPointPosition.y - _prevMousePosY) * _variation;
+            // オブジェクトの座標を変化させる
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + difference, this.transform.position.z);
+            // 前フレームの座標を保存
+            _prevMousePosY = screenToWorldPointPosition.y;
         }
     }
 }
