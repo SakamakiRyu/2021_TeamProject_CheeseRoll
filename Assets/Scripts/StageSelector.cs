523757438f471a48385d4773a409ebe273cd40f0
jackblack0094 <syomzn10@gmail.com>
2021-10-22 20:16:20 +0900

    スワイプを作成

--- Assets/Scripts/StageSelector.cs
+++ Assets/Scripts/StageSelector.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class StageSelector : MonoBehaviour
 {
+    private Vector3 _pos, currentPos;
+    [SerializeField]
+    GameObject _panelPages;
+    [SerializeField]
+    float _sensitivity = 1; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,11 +21,15 @@ public class StageSelector : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            
+            _pos = Input.mousePosition;
         }
         if (Input.GetMouseButton(0))
         {
-
+            currentPos = Input.mousePosition;
+            Vector3 diffDistance = currentPos - _pos;
+            diffDistance *= _sensitivity;
+            _panelPages.transform.position += new Vector3(diffDistance.x, 0);
+            _pos = Input.mousePosition;
         }
     }
 }
