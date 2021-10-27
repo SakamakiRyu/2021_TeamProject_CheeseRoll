3e12c0384be10e7283652713eb4da789dd27bca2
SakamakiRyu <potesara031w@gmail.com>
2021-10-28 00:36:44 +0900

    UpDownByFingerに、スムージング機能を追加した

--- Assets/Scripts/InGame/UpDownByFinger.cs
+++ Assets/Scripts/InGame/UpDownByFinger.cs
@@ -1,129 +1,81 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
-/// スマホの上下スワイプ操作で、オブジェクトの座標を操作する
+/// スマホの上下スワイプ操作で、鉄板の生成位置を操作する
 /// </summary>
 public class UpDownByFinger : MonoBehaviour
 {
-    //オブジェクトのy座標の下限値
+    [Header("追従速度")]
+    [SerializeField]
+    private float _chaseSpeed;
+
+    [Header("減衰力")]
+    [SerializeField]
+    private float _attenuation;
+
+    [Header("追従するオブジェクトの座標")]
+    [SerializeField]
+    private Transform _targetTransform;
+
+    [Header("鉄板生成の下限値( y座標 )")]
     [SerializeField]
     private float _min;
 
-    //オブジェクトのy座標の上限値
+    [Header("鉄板生成の上限値( y座標 )")]
     [SerializeField]
     private float _max;
 
-    [Header("変化量にかける最大倍率")]
-    [SerializeField]
-    private float _variationMaxValue;
-
-    [Header("変化量にかける最小倍率")]
-    [SerializeField]
-    private float _variationMinValue;
-
-    [Header("変化をかける最小値")]
-    [SerializeField]
-    private float _maxChengeValue = 0f;
-
-    [Header("変化をかける最大値")]
-    [SerializeField]
-    private float _minChengeValue = 0f;
-
-    /// <summary>変化量</summary>
-    private float _chengeValue;
-
-    /// <summary>前のフレームのy座標を保存しておく</summary>
+    /// <summary>前のフレームのy座標を保存しておく変数</summary>
     private float _prevMousePosY;
 
-    /// <summary>画面に触っているかのフラグ</summary>
-    private bool _isTouched = default;
-
-    /// <summary>経過時間を保存する変数</summary>
-    private float _time = 0f;
+    private Vector3 _velo;
 
     private void Update()
     {
         Control();
     }
 
-    /// <summary>スマホの上下のスワイプ操作にて、オブジェクトの座標を変更する(↑にスワイプ=上昇)</summary>
+    /// <summary>スマホの上下のスワイプ操作にて、生成位置を変更する(↑にスワイプ=上昇)</summary>
     void Control()
     {
         Vector3 pos;
         Vector3 screenToWorldPointPosition;
 
-        
+        // Vector3でマウスの位置座標を取得
+        pos = Input.mousePosition;
+        // Z軸修正
+        pos.z = 10f;
+        // マウスの位置座標からスクリーン座標に変換する
+        screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(pos);
+        // 前フレームとの差分を保存する
+        var differenceValue = (screenToWorldPointPosition.y - _prevMousePosY);
 
-        if (Input.GetButtonDown("Fire1"))
+        // ターゲット座標の設定
+        if (_targetTransform.position.y >= _min && _targetTransform.position.y <= _max)
         {
-            _isTouched = true;
-            // Vector3でマウスの位置座標を取得
-            pos = Input.mousePosition;
-            // Z軸修正
-            pos.z = 10f;
-            // マウス位置座標からスクリーン座標に変換する
-            screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(pos);
-            // タッチしたフレーム時の座標を保存
-            _prevMousePosY = screenToWorldPointPosition.y;
+            _targetTransform.position = new Vector3(this.transform.position.x, _targetTransform.position.y + differenceValue, this.transform.position.z);
         }
 
-        // 画面に触れていない場合は何もしない
-        if (!_isTouched) return;
-
-        if (Input.GetButtonUp("Fire1"))
+        // 補正(ターゲットのy座標が設定した下限値を下まわった場合は、下限値に修正する)
+        if (_targetTransform.position.y < _min)
         {
-            _isTouched = false;
-            return;
+            _targetTransform.position = new Vector3(_targetTransform.position.x, _min, _targetTransform.position.z);
+        }
+        // (ターゲットのy座標が設定した上限値を上まわった場合は、上限値に修正する)
+        else if (_targetTransform.position.y > _max)
+        {
+            _targetTransform.position = new Vector3(_targetTransform.position.x, _max, _targetTransform.position.z);
         }
 
-        if (_isTouched)
-        {
-            _time += Time.deltaTime;
+        // y座標の保存
+        _prevMousePosY = screenToWorldPointPosition.y;
 
-            // Vector3でマウスの位置座標を取得
-            pos = Input.mousePosition;
-            // Z軸修正
-            pos.z = 10f;
-            // マウスの位置座標からスクリーン座標に変換する
-            screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(pos);
-            // 前フレームとの差分を保存する
-            var differenceValue = (screenToWorldPointPosition.y - _prevMousePosY) * _variationMaxValue;
-            // 差分を変価値に加算する
-            _chengeValue += differenceValue;
-            // 変化量の絶対値を計算
-            var abs = Mathf.Abs(differenceValue);
-            // 値が、最大変化量を超えた場合は補正する
-            if (abs > _maxChengeValue)
-            {
-                if (differenceValue < 0)
-                {
-                    differenceValue = _maxChengeValue * -1;
-                }
-                else
-                {
-                    differenceValue = _maxChengeValue;
-                }
-            }
+        // ターゲット座標の変更
+        _targetTransform.position = new Vector3(this.transform.position.x, _targetTransform.position.y, this.transform.position.z);
 
-            if (this.transform.position.y >= _min && this.transform.position.y <= _max)
-            {
-                // this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + _chengeValue, this.transform.position.z);
-            }
-
-            // 補正
-            if (this.transform.position.y < _min)
-            {
-                this.transform.position = new Vector3(this.transform.position.x, _min, this.transform.position.z);
-            }
-            if (this.transform.position.y > _max)
-            {
-                this.transform.position = new Vector3(this.transform.position.x, _max, this.transform.position.z);
-            }
-
-            // 座標を保存
-            _prevMousePosY = screenToWorldPointPosition.y;
-        }
+        // 生成位置をターゲット座標に追従させる
+        _velo += (_targetTransform.position - this.transform.position) * _chaseSpeed;
+        _velo *= _attenuation;
+        this.transform.position += _velo *= Time.deltaTime;
     }
 }
