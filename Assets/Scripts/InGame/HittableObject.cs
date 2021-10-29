3ff6a9bf73b4eb83a7494a5f723954ba6a97aac3
syun00613203 <83343174+syun00613204@users.noreply.github.com>
2021-10-29 20:13:06 +0900

    食べ物を Gameシーン に追加 ぶつかると Log が出て消える

---
+++ Assets/Scripts/InGame/HittableObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HittableObject : MonoBehaviour
+{
+    private enum HittableObjectType
+    {
+        Food = 0,            // 食べ物
+        Enemy = 100,           // 敵
+        HittableObject = 150,　// 障害物
+    }
+
+    [SerializeField] string _hitJudgeTag = "Player";
+    [SerializeField] HittableObjectType _objectType;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == _hitJudgeTag)
+        {
+            if(100 <= (int)_objectType)
+            {
+                Debug.Log("Hit " + this.name);
+            }
+            else
+            {
+                Debug.Log("Food " + this.name);
+            }
+
+
+            Destroy(this.gameObject, 0.1f);
+        }
+    }
+}
