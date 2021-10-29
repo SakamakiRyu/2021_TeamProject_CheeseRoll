b5127f0b3b1d9d5f88c63151111400610c956306
D-Itsuki <raito.kaminari@gmail.com>
2021-10-29 20:20:51 +0900

    UIのポップアップ関数とアニメーションの作成

---
+++ Assets/Scripts/StagePopupController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StagePopupController : MonoBehaviour
+{
+    [SerializeField] string[] _texts;
+    [SerializeField] Sprite[] _sprites;
+    [SerializeField] Text _stageText;
+    [SerializeField] Image _stageImage;
+    Animator _anim;
+    bool _isPop;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _isPop = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PopUp(0);
+        }
+    }
+
+    public void PopUp(int id)
+    {
+        _stageText.text = _texts[id];
+        _stageImage.sprite = _sprites[id];
+        if (!_isPop)
+        {
+            _anim.SetTrigger("In");
+            _isPop = true;
+        }
+        else
+        {
+            _anim.SetTrigger("Out");
+            _isPop = false;
+        }
+    }
+}
