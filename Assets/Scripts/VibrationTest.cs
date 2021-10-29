daff3acf4c5ca527fdab05c0f62d6b790eebe47a
yayoi <fumi7214@gmail.com>
2021-10-29 20:25:10 +0900

    バイブレーション機能の追加

---
+++ Assets/Scripts/VibrationTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibrationTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Test()
+    {
+        VibrationManager.Vibration(1000);
+    }
+}
