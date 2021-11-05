0f16d1c828feaeb17712d62e263f7505f8d5688e
jackblack0094 <syomzn10@gmail.com>
2021-11-05 16:56:57 +0900

    トリガーのタグを追加　ステージセレクトのシーン（仮）を制作

---
+++ Assets/StageSelectPlayerCon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectPlayerCon : MonoBehaviour
+{
+    [SerializeField]
+    public StagePopupController _StagePopupController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * 10f * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * 10f * Time.deltaTime;
+        }
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Stage trigger"))
+        {
+            
+            var namae = collision.GetComponent<stageID>();
+            _StagePopupController.PopUp((int)namae._id);
+            Debug.Log("in");
+        }
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.CompareTag("Stage trigger"))
+        {
+            Debug.Log("out");
+        }
+    }
+}
---
+++ Assets/stageID.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stageID : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ID _id;
+    public enum ID
+    {
+        test,
+        one,
+        two,
+        three,
+        four,
+        five,
+        six,
+    }
+
+
+}
