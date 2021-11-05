c89073bfabcdc827b20cc440ae3fc124ad5854d2
syun00613203 <83343174+syun00613204@users.noreply.github.com>
2021-11-05 18:18:36 +0900

    アツアツ状態のための BurningCheese を作成 HittableObject に変更

---
+++ Assets/Scripts/InGame/BurningCheese.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurningCheese : MonoBehaviour
+{
+    [System.Serializable]
+    struct Burns
+    {
+        [SerializeField] string name;
+        [SerializeField] float time;
+
+        public string Name { get => name;}
+        public float Time { get => time;}
+    }
+
+    [SerializeField] Burns[] _burns;
+    [SerializeField] float _timer;
+    float _maxTime;
+ 
+    bool _burning = false;
+
+    [SerializeField] GameObject _testObj;
+
+    /// <summary>
+    /// アツアツ状態か否か を取得
+    /// </summary>
+    public bool Burning { get => _burning;}
+
+    private void Update()
+    {
+        if (_burning)
+        {
+            _timer += Time.deltaTime;
+
+            if (_timer > _maxTime)
+            {
+                _burning = false;
+                _timer = 0;
+
+                if (_testObj != null)
+                {
+                    _testObj.SetActive(false);
+                }
+            }
+        }
+    }
+
+    //  唐辛子につけてる チーズから呼び出したい？？
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="name"> HittableObj の Name </param>
+    public void StartBurn(string name)
+    {
+        //Burns nowBurn;
+        for (int i = 0; i < _burns.Length; i++)
+        {
+            if (name == _burns[i].Name)
+            {
+                //nowBurn = _burns[i];
+
+                _maxTime = _burns[i].Time;
+                _burning = true;
+
+                if (_testObj != null)
+                {
+                    _testObj.SetActive(true);
+                }
+            }
+        }
+
+
+
+    }
+}
--- Assets/Scripts/InGame/HittableObject.cs
+++ Assets/Scripts/InGame/HittableObject.cs
@@ -13,37 +13,62 @@ public class HittableObject : MonoBehaviour
         HittableObject = 150,　// 障害物
     }
 
+    [SerializeField] string _name;
+
+    // Fire tag を作って食材を燃やす?
+
     [SerializeField] string _hitJudgeTag = "Player";
     [SerializeField] HittableObjectType _objectType;
 
+    [SerializeField] UnityEngine.Events.UnityEvent _onItemPickup;
+
+    public string Name { get => _name;}
+
     //private void Start()
     //{
-        
+
     //}
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == _hitJudgeTag)
         {
-            if((int)_objectType >= 150)
-            {
-                Debug.Log($"Hit {this.name} {_objectType}");
-            }
-            else if((int)_objectType >= 100)
-            {
-                Debug.Log($"Hit {this.name} {_objectType}");
-            }
-            else if((int)_objectType >= 80)
-            {
-                Debug.Log($"Ge {this.name} {_objectType}");
-            }
-            else
-            {
-                Debug.Log($"Food {this.name} {_objectType}");
-            }
+            _onItemPickup.Invoke();
 
+            //DebugItem1();
 
-            Destroy(this.gameObject, 0.1f);
+            //DestroyThis();
         }
+    }
+
+    /// <summary>
+    /// Debug.Log を出す。GameObjectの名前、Nameに入ってる単語、オブジェクトタイプが出る
+    /// </summary>
+    public void DebugItem1()
+    {
+        if ((int)_objectType >= 150)
+        {
+            Debug.Log($"Hit {this.name} {Name} {_objectType}");
+        }
+        else if ((int)_objectType >= 100)
+        {
+            Debug.Log($"Burning {this.name} {Name} {_objectType}");
+        }
+        else if ((int)_objectType >= 80)
+        {
+            Debug.Log($"Recoverey {this.name} {Name} {_objectType}");
+        }
+        else
+        {
+            Debug.Log($"Food {this.name} {Name} {_objectType}");
+        }
+    }
+
+    /// <summary>
+    /// 0.1f 後にGameObjectを破壊
+    /// </summary>
+    public void DestroyThis()
+    {
+        Destroy(this.gameObject, 0.1f);
     }
 }
