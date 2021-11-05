93651aa59e529588c100ed3e0057dfc907c466a7
D-Itsuki <raito.kaminari@gmail.com>
2021-11-05 20:17:08 +0900

    ステージ選択のプレイヤーのアニメーターを作成

---
+++ Assets/Scripts/StageSelect/StageSelectPlayerAnimationController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectPlayerAnimationController : MonoBehaviour
+{
+    public static StageSelectPlayerAnimationController Instance;
+
+    [SerializeField]
+    Animator _animator = null;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    /// <summary>
+    /// 進行方向
+    /// </summary>
+    public enum Move
+    {
+        Right,
+        Left
+    }
+
+    /// <summary>
+    /// 進行方向を受け取って、アニメーターに渡す
+    /// </summary>
+    /// <param name="nextPosition">進行方向</param>
+    public void OnMove(Move nextPosition)
+    {
+        switch (nextPosition)
+        {
+            case Move.Right:
+                _animator.SetTrigger("Right");
+
+                break;
+            case Move.Left:
+                _animator.SetTrigger("Left");
+                break;
+            default:
+                break;
+        }
+
+    }
+}
