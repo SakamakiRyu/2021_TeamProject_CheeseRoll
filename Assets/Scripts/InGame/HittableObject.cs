836c429cacefa7a16b98cf40c2ba02dcfc5e3008
syun00613203 <83343174+syun00613204@users.noreply.github.com>
2021-11-05 10:27:26 +0900

    食べ物などと背景をプレハブにして配置

--- Assets/Scripts/InGame/HittableObject.cs
+++ Assets/Scripts/InGame/HittableObject.cs
@@ -7,29 +7,39 @@ public class HittableObject : MonoBehaviour
     private enum HittableObjectType
     {
         Food = 0,            // 食べ物
-        Enemy = 100,           // 敵
+        Recoverey = 80,      // 回復
+        RedHeat = 100,          // 唐辛子
+        Burn = 110,             // 焦げた
         HittableObject = 150,　// 障害物
     }
 
     [SerializeField] string _hitJudgeTag = "Player";
     [SerializeField] HittableObjectType _objectType;
 
-    private void Start()
-    {
+    //private void Start()
+    //{
         
-    }
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == _hitJudgeTag)
         {
-            if(100 <= (int)_objectType)
+            if((int)_objectType >= 150)
             {
-                Debug.Log("Hit " + this.name);
+                Debug.Log($"Hit {this.name} {_objectType}");
+            }
+            else if((int)_objectType >= 100)
+            {
+                Debug.Log($"Hit {this.name} {_objectType}");
+            }
+            else if((int)_objectType >= 80)
+            {
+                Debug.Log($"Ge {this.name} {_objectType}");
             }
             else
             {
-                Debug.Log("Food " + this.name);
+                Debug.Log($"Food {this.name} {_objectType}");
             }
 
 
