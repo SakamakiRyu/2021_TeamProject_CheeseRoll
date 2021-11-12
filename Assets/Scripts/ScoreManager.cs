a3384bc83e3ed16d11b23bd8d3f8d190a846d256
yayoi <fumi7214@gmail.com>
2021-11-12 17:46:55 +0900

    スコア機能のアップデートとロードマテリアルの追加

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 /// <summary>
@@ -9,19 +10,21 @@ public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance;
 
-    ///<summary>スコアの最大値</summary>
-    public int MaxScoreValue = 10;
+    public Score ScoreStructure;
 
-    [SerializeField]
-    ///<summary>現在のスコア</summary>
-    private int _score = 0;
+    private int[] _dishesScore = { 10, 30, 60 };
 
-    ///<summary>もし想定していた値を超えていても最大値を渡す</summary>
-    public int Score
-    {
-        get => Mathf.Clamp(_score, 0, MaxScoreValue);
-        set => _score = Mathf.Clamp(value, 0, MaxScoreValue);
-    }
+    //↓これをステージ内のスクリプトでインスタンスと初期化をしてください
+    /*/
+     * ステージ内の食材
+     string[] foods = {
+             "Tomato",
+             "Onion",
+         };
+
+         int[] nums = new int[foods.Length];
+         ScoreManager.Instance._scoreStructure = new ScoreManager.Score() { _FoodsList = foods, _FoodsNums = nums, _ScoreUI = FindObjectOfType<ScoreUI>(), _Dishes = 3 };
+     /*/
     private void Awake()
     {
         MakeSingle();
@@ -39,12 +42,93 @@ public class ScoreManager : MonoBehaviour
             Destroy(this);
         }
     }
-    /// <summary>
-    /// 受け取った値をスコアに加算する
-    /// </summary>
-    /// <param name="plusScore">加算する値</param>
-    public void ScorePlus(int plusScore)
+   
+
+    public struct Score
     {
-        Mathf.Clamp(_score += plusScore, 0, MaxScoreValue);//最大値から出ないようにする
+        ///<summary>ステージの食材の配列</summary>
+        public string[] FoodsList { get; set; }
+        ///<summary>食材の獲得数</summary>
+        public int[] FoodsNums { get; set; }
+        ///<summary>何人前か？</summary>
+        public int Dishes { get; set; }
+        ///<summary>スコアのUIのコンポーネント</summary>
+        public ScoreUI ScoreUI { get; set; }
+        ///<summary>獲得した焦げた具材の数</summary>
+        public int BurntFoodCount { get; set; }
+
+        public Score(string[] foodsList,int[] foodsNums,int dishes,ScoreUI scoreUI)
+        {
+            this.FoodsList = foodsList;
+            this.FoodsNums = foodsNums;
+            this.Dishes = dishes;
+            this.ScoreUI = scoreUI;
+            this.BurntFoodCount = 0;
+        }
+        /// <summary>
+        /// スコアを加算する
+        /// 食材を獲得した場合呼び出してください
+        /// </summary>
+        /// <param name="food">獲得した食材</param>
+        public void ScoreUp(string food)
+        {
+            for (int i = 0; i < FoodsList.Length; i++)
+            {
+                if (FoodsList[i] == food)
+                {
+                    FoodsNums[i]++;
+                    ScoreUI.ScoreUpUi(food);
+                }
+            }
+        }
+        /// <summary>
+        /// 焦げた食材を取得したときに呼ばれて焦げた食材のカウントが増える
+        /// 焦げた食材を獲得した場合呼び出してください
+        /// </summary>
+        public void BurntFoodCountUp()
+        {
+
+            BurntFoodCount++;
+            ScoreUI.ScoreUpUi("BurntFood");
+
+        }
+       
+    }
+    /// <summary>
+    /// 最終的なスコアを計算する
+    /// </summary>
+    /// <returns>最終的なスコア</returns>
+    public int ScoreCalculation()
+    {
+        if (Instance.ScoreStructure.FoodsNums == null)
+        {
+            return 0;
+        }
+        int bonus = 0;//何個とったか？
+        Instance.ScoreStructure.FoodsNums.ToList().ForEach(x => bonus += x);
+
+        int dishes = 0;//何人前か？
+        bool isBreak = false;//trueなら抜け出す
+        for (int i = 0; i < Instance.ScoreStructure.Dishes; i++)
+        {
+            for (int k = 0; k < Instance.ScoreStructure.FoodsNums.Length; k++)
+            {
+                Instance.ScoreStructure.FoodsNums[k]--;
+                if (Instance.ScoreStructure.FoodsNums[k] > 0)
+                {
+                    isBreak = true;
+                    break;
+
+                }
+
+            }
+            if (isBreak)
+            {
+                break;
+            }
+            dishes = _dishesScore[i];
+        }
+
+        return bonus * dishes - Instance.ScoreStructure.BurntFoodCount;//最終的なスコア
     }
 }
---
+++ Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour
+{
+
+    [SerializeField]
+     GameObject _foodImage;
+
+    [SerializeField]
+     GameObject _foodPanelUi;
+
+    List<GameObject> _foods = new List<GameObject>();
+
+    private void Start()
+    {      
+        ScoreUiSetup();
+    }
+    /// <summary>
+    /// ステージの食材をUIに設定する
+    /// </summary>
+    void ScoreUiSetup()
+    {
+        for (int i = 0; i < ScoreManager.Instance.ScoreStructure.FoodsList.Length; i++)
+        {
+            _foods.Add(Instantiate(_foodImage, _foodPanelUi.transform));
+            _foods[i].name = ScoreManager.Instance.ScoreStructure.FoodsList[i];
+        }
+
+        _foods.Add(Instantiate(_foodImage, _foodPanelUi.transform));
+        _foods[ScoreManager.Instance.ScoreStructure.FoodsList.Length].name = "BurntFood";
+    }
+    /// <summary>
+    /// 獲得した食材のUIに反映する
+    /// </summary>
+    /// <param name="food">獲得した食材</param>
+    public void ScoreUpUi(string food)
+    {
+        for (int i = 0; i < ScoreManager.Instance.ScoreStructure.FoodsList.Length; i++)
+        {
+            if (food == _foods[i].name)
+            {
+                _foods[i].GetComponentInChildren<Text>().text = $"×{ScoreManager.Instance.ScoreStructure.FoodsNums[i]}";
+            }
+        }
+    }
+}
