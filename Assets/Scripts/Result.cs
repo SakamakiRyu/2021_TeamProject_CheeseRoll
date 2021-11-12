9a601848fa1ca0159baea7870f12cb5ecfa43d71
yayoi <fumi7214@gmail.com>
2021-11-12 17:55:25 +0900

    リザルトの機能のアップデート

--- Assets/Scripts/Result.cs
+++ Assets/Scripts/Result.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Timeline;
 using UnityEngine.UI;
+using System.Linq;
 public class Result : MonoBehaviour
 {
     [SerializeField]
@@ -11,32 +12,32 @@ public class Result : MonoBehaviour
 
     [SerializeField]
     ///<summary>スコアを表示するオブジェクト</summary>
-    Image[] _scoreEvaluationUiImage;  
+    Image[] _scoreEvaluationUiImage;
     [SerializeField]
     ///<summary>タイムラインを制御するオブジェクト</summary>
     TimeLineManager _timeLineManager;
 
+    [SerializeField]
+    Text[] _sterText;
+
+    ///<summary>ステージの評価スコアの基準値</summary>
+    public int[] EvaluationScore = {
+            5000,
+            10000,
+            15000,
+            30000,
+            50000
+    };
+
+    int _scoreValue;
     void Start()
     {
+        _scoreValue = ScoreManager.Instance.ScoreCalculation();
         _timeLineManager.PlayTimeLine(0);
-        if (ScoreManager.Instance.Score <= 3)
-        {
-            _scoreEvaluationUiImage[0].gameObject.SetActive(true);
-        }
-        else if (ScoreManager.Instance.Score <= 6)
-        {
-            _scoreEvaluationUiImage[0].gameObject.SetActive(true);
-            _scoreEvaluationUiImage[1].gameObject.SetActive(true);
-        }
-        else
-        {
-            _scoreEvaluationUiImage[0].gameObject.SetActive(true);
-            _scoreEvaluationUiImage[1].gameObject.SetActive(true);
-            _scoreEvaluationUiImage[2].gameObject.SetActive(true);
-        }
+
     }
 
-   
+
     /// <summary>
     /// シグナルに設定するための関数
     /// </summary>
@@ -50,15 +51,70 @@ public class Result : MonoBehaviour
     public IEnumerator ScoreAnim()
     {
         int value = 0;
-        while (ScoreManager.Instance.Score >= value)
+        while (_scoreValue >= value)
         {
 
-            _scoreTextUi.text = $"具材獲得数　 :　 {value}　/　{ScoreManager.Instance.MaxScoreValue}";
+            _scoreTextUi.text = $"スコア　 :　 {value}　/　{10000000}";
             value++;
 
-            yield return new WaitForSeconds(0.2f);
+            yield return new WaitForSeconds(0.01f);
+        }
+        StartCoroutine(evaluationAnim());
+    }
+    IEnumerator evaluationAnim()
+    {
+       
+        if (EvaluationScore[4] <= _scoreValue)
+        {
+            yield return new WaitForSeconds(1.0f);
+            _sterText[0].text = "★";
+            yield return new WaitForSeconds(1.0f);
+            _sterText[1].text = "★";
+            yield return new WaitForSeconds(1.0f);
+            _sterText[2].text = "★";
+        }
+        else if (EvaluationScore[3] <= _scoreValue)
+        {
+            yield return new WaitForSeconds(1.0f);
+            _sterText[0].text = "★";
+            yield return new WaitForSeconds(1.0f);
+            _sterText[1].text = "★";
+            yield return new WaitForSeconds(1.0f);
+            _sterText[2].text = "☆";
+        }
+        else if (EvaluationScore[2] <= _scoreValue)
+        {
+            yield return new WaitForSeconds(1.0f);
+            _sterText[0].text = "★";
+            yield return new WaitForSeconds(1.0f);
+            _sterText[1].text = "★";
+            yield return new WaitForSeconds(1.0f);
+            _sterText[2].text = "";
+        }
+        else if (EvaluationScore[1] <= _scoreValue)
+        {
+            yield return new WaitForSeconds(1.0f);
+            _sterText[0].text = "★";
+            yield return new WaitForSeconds(1.0f);
+            _sterText[1].text = "☆";
+            yield return new WaitForSeconds(1.0f);
+            _sterText[2].text = "";
+        }
+        else if (EvaluationScore[0] <= _scoreValue)
+        {
+            yield return new WaitForSeconds(1.0f);
+            _sterText[0].text = "★";
+            yield return new WaitForSeconds(1.0f);
+            _sterText[1].text = "";
+            yield return new WaitForSeconds(1.0f);
+            _sterText[2].text = "";
+        }
+        else
+        {
+            _sterText[0].text = "";
+            _sterText[1].text = "";
+            _sterText[2].text = "";
         }
     }
 
-   
 }
