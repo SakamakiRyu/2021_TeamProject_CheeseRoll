7acd56a03acda32fba8e25cba59ac90fbf3e2272
kojima0725 <kojikoji0725kt@gmail.com>
2022-01-05 04:54:24 +0900

    エフェクト再生機能

--- Assets/Scripts/Managers/EffectManager.cs
+++ Assets/Scripts/Managers/EffectManager.cs
@@ -11,10 +11,21 @@ using Cinemachine;
 //[RequireComponent(typeof(CinemachineImpulseSource))]
 public class EffectManager : MonoBehaviour
 {
+    public enum EffectType
+    {
+        GetItem,
+        Cure,
+    }
+
     public static EffectManager Instance { get; private set; }
 
     [SerializeField]
     private CinemachineImpulseSource _ImpluseSource;
+
+    [Header("EffectTypeのenumと同じ順番に入れてください")]
+    [SerializeField]
+    private GameObject[] _effectPrefabs;
+    
 
     private void Awake()
     {
@@ -22,16 +33,40 @@ public class EffectManager : MonoBehaviour
         {
             Instance = this;
         }
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     /// <summary>
     /// カメラシェイクをする
     /// </summary>
-    public void PlayCameraShake()
+    public void PlayCameraShake(float size)
     {
         if (_ImpluseSource)
         {
-            _ImpluseSource?.GenerateImpulse();
+            _ImpluseSource?.GenerateImpulse(size);
         }
+    }
+
+    /// <summary>
+    /// カメラシェイクをする
+    /// </summary>
+    public void PlayCameraShake(Vector3 velocity)
+    {
+        if (_ImpluseSource)
+        {
+            _ImpluseSource?.GenerateImpulse(velocity);
+        }
+    }
+
+    public GameObject PlayEffect(EffectType type, Vector3? localPosition = null, Transform parent = null)
+    {
+        Vector3 pos = localPosition ?? Vector3.zero;
+        GameObject effect = Instantiate(_effectPrefabs[(int)type]);
+        effect.transform.parent = parent;
+        effect.transform.localPosition = pos;
+        return effect;
     }
 }
